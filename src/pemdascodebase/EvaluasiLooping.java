6d1c9fb3bd48e5697c57e58904f2bfcaaace3161
DevianWahyu <prof.devian.wahyu@gmail.com>
2020-11-25 10:26:04 +0700

    Add jawaban evaluasi looping

---
+++ src/pemdascodebase/EvaluasiLooping.java
@@ -0,0 +1,67 @@
+package pemdascodebase;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author devianwahyu
+ */
+
+import java.util.Scanner;
+
+public class EvaluasiLooping {
+
+    public static void main(String[] args) {
+
+//        Soal 1
+//        Scanner in = new Scanner(System.in);
+//        
+//        int n = in.nextInt();
+//        int bil1 = 0, bil2 = 1;
+//
+//        for (int i = 1; i <= n; ++i)
+//        {
+//            System.out.print(bil1+" ");
+//            int sum2BilanganSebelum = bil1 + bil2;
+//            bil1 = bil2;
+//            bil2 = sum2BilanganSebelum;
+//        }System.out.println("");
+
+//        Soal 2
+//        Scanner in = new Scanner(System.in);
+//
+//        int inputUser = in.nextInt();
+//        int genap = 0, ganjil = 0;
+//
+//        for (int i = 0; i <= inputUser; i++) {
+//            if (i % 2 == 0 ) {
+//                genap++;
+//            } else {
+//                ganjil++;
+//            }
+//        }
+//        
+//        System.out.println("Jumlah angka genap = " + genap);
+//        System.out.println("Jumlah angka ganjil = " + ganjil);
+
+//        Soal 3
+//        Scanner in = new Scanner(System.in);
+//        
+//        int inputUser = in.nextInt();
+//        
+//        for (int i = inputUser; i > 0; i--) {
+//            for (int j = i; j < inputUser; j++) {
+//                System.out.print("  ");
+//            }
+//            for (int k = 0; k < i; k++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println("");
+//        } 
+    }
+
+}
