47ed0d212a8bdd747c13dbb61351d1c5a4ebdddd
DevianWahyu <prof.devian.wahyu@gmail.com>
2020-12-16 05:21:55 +0700

    Update SeleksiKondisi.java

--- src/pemdascodebase/SeleksiKondisi.java
+++ src/pemdascodebase/SeleksiKondisi.java
@@ -17,8 +17,8 @@ public class SeleksiKondisi {
 
         /*
         
-        Percabangan
-        Di Java percabangan dapat diatasi dengan beberapa alternatif:
+        Seleksi Kondisi
+        Di Java Seleksi Kondisi dapat dilakukan dengan beberapa alternatif:
         1. if
         2. switch case
         3. conditional assignment
@@ -32,17 +32,19 @@ public class SeleksiKondisi {
             } else {
                 statement;
             }
+
         2. switch case
             switch(conditional_variabel) {
-                case 1:
+                case nilaiPembanding:
                     statement;
                     break;
-                case n:
+                case nilaiPembandingN:
                     statement;
                     break;
                 default:
                     statement;
             }
+
         3. conditional assignment
             data_type variable_name = (condition) ? true_statement : false_statement;
         
@@ -56,24 +58,56 @@ public class SeleksiKondisi {
         
          */
 
-        /*
-        
-        Kasus 1
-        Budi naik kelas jika nilainya lebih dari 70
 
-        Kasus 2
-        Budi dapat nilai A jika ujiannya diatas 80, nilai B jika ujiannya diatas 70, dan nilai C jika diatas 60. 
-        Jika dibawah 60 maka nilainya D.
+        
+//        Kasus 1
+//        Budi naik kelas jika nilainya lebih dari 70
 
-        Kasus 3
-        Berat badan Budi ideal jika lebih dari 60kg dan dibawah 65kg.
+        // Jawaban soal 1
+//        Scanner in = new Scanner(System.in);
+//        int nilai = in.nextInt();
+//
+//        String naikKelas = nilai > 70 ? "Naik Kelas" : "Tidak Naik Kelas";
+//
+//        System.out.println(naikKelas);
 
-        Kasus 4
-        Budi disuruh konversi angka jadi hari, misal 1 = minggu, 2 = senin, dst
-        
-        */
-        
-        
-    }
+//        Kasus 2
+//        Budi dapat nilai A jika ujiannya diatas 80, nilai B jika ujiannya diatas 70, dan nilai C jika diatas 60.
+//        Jika dibawah 60 maka nilainya D.
+//        String nilaiHuruf = "";
+//
+//        if (nilai >=0 && nilai <= 100) {
+//            if (nilai > 80) {
+//                nilaiHuruf = "A";
+//            } else if (nilai > 70) {
+//                nilaiHuruf = "B";
+//            } else if (nilai > 60) {
+//                nilaiHuruf = "C";
+//            } else {
+//                nilaiHuruf = "D";
+//            }
+//        } else {
+//            nilaiHuruf = "Nilai diluar jangkauan";
+//        }
+//
+//        System.out.println(nilaiHuruf);
+
+//        Kasus 3
+//        Budi disuruh konversi angka jadi hari, misal 1 = minggu, 2 = senin, dst
+//        int hariAngka = in.nextInt();
+//        String hariHuruf;
+//        switch (hariAngka) {
+//            case 1:
+//                hariHuruf = "Minggu";
+//                break;
+//            case 2:
+//                hariHuruf = "Senin";
+//                break;
+//            default:
+//                hariHuruf = "Hari tidak ditemukan";
+//        }
+//        System.out.println(hariHuruf);
+//
+//    }
 
 }
